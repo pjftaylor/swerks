159c6f8a2ddede871ec85575b6410f156a6fab16
Patrick <pjftaylor@gmail.com>
2017-11-29 12:25:52 +1100

    Added working solution for deletion

--- todo/write_read_dynamodb.rb
+++ todo/write_read_dynamodb.rb
@@ -7,16 +7,18 @@ require 'aws-sdk-dynamodb'  # v2: require 'aws-sdk'
 
 puts "Enter a test value to be added to the DB, then press RETURN"
 
-DEValue = gets.chomp
+addValue = gets.chomp
 
 dynamoDB = Aws::DynamoDB::Resource.new(region: 'us-east-1')
 
 table = dynamoDB.table('TestTable')
 
+###########################################
+
 table.put_item({
   item:
     {
-      "TestKey" => DEValue
+      "TestKey" => addValue
 }})
 
 scan_output = table.scan({
@@ -32,11 +34,35 @@ scan_output.items.each do |item|
   end
 end
 
+############################################
 
+puts "Press RETURN to continue"
+x = gets
 
+puts "Enter a test value to be REMOVED to the DB, then press RETURN"
 
+remValue = gets.chomp
 
+params = {
+    table_name: 'TestTable',
+    key: {
+        TestKey: remValue
+    }
+}
+table.delete_item(params)
+puts 'Deleted the selected item'
 
+scan_output = table.scan({
+  limit: 50,
+  select: "ALL_ATTRIBUTES"
+})
 
+scan_output.items.each do |item|
+  keys = item.keys
+
+  keys.each do |k|
+    puts "#{k}: #{item[k]}"
+  end
+end
 
 
